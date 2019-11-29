0584aa73e426bd59e016abe1c1dda8e22c305e44
samy-ib <samy.ib@protonmail.com>
2019-11-29 12:21:17 +0100

    testing

--- common/nn_class.py
+++ common/nn_class.py
@@ -33,15 +33,15 @@ class Net(nn.Module):
 
 
 def train(network, optimizer, criterion, trainloader, validloader, testloader, EPOCHS):
-    # network.cuda()
+    network.cuda()
     train_log=[]
     valid_log=[]
     for epoch in range(EPOCHS):
         training_loss = 0
         network.train() #Set the network to training mode
         for X, Y in trainloader:
-            # X=X.cuda()
-            # Y=Y.cuda()
+            X=X.cuda()
+            Y=Y.cuda()
             optimizer.zero_grad()
             out = network(X)
             loss = criterion(out, Y)
@@ -69,8 +69,8 @@ def valid(network, criterion, validloader):
     with torch.no_grad(): #Desactivate autograd engine (reduce memory usage and speed up computations)
         network.eval() #set the layers to evaluation mode(batchnorm and dropout)
         for X, Y in validloader:
-            # X=X.cuda()
-            # Y=Y.cuda()
+            X=X.cuda()
+            Y=Y.cuda()
             out = network(X)
             loss = criterion(out, Y)
             validation_loss += loss.item()
@@ -91,8 +91,8 @@ def test(network, testloader):
     with torch.no_grad(): #Desactivate autograd engine (reduce memory usage and speed up computations)
         network.eval() #set the layers to evaluation mode(batchnorm and dropout)
         for X, Y in testloader:
-            # X=X.cuda()
-            # Y=Y.cuda()
+            X=X.cuda()
+            Y=Y.cuda()
             out = network(X)
 
             predict = network(X)
