7aa7fb04b704caa854f2d5553d5122ef5716ccf6
samy-ib <samy.ib@protonmail.com>
2019-12-22 10:33:46 +0100

    bigger architecture and train nsp on GPU

--- common/nn_nsp.py
+++ common/nn_nsp.py
@@ -59,7 +59,7 @@ def train(network, optimizer, criterion, trainloader, validloader, testloader, E
         validation_loss, accuracy = valid(network, criterion, validloader)
 
         if validation_loss<best_loss:
-            torch.save(network.state_dict(), 'models/nsp_model.pt')
+            torch.save(network.state_dict(), 'models/nsp_model_epoch'+epoch+ '_loss' +"%.3f"%loss +'.pt')
             best_loss=validation_loss
 
         log(EPOCHS, epoch, training_loss, validation_loss, accuracy)
